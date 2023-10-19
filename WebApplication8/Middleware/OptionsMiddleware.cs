2a4a5c1f2665c9ddcedff5ba4e62158e7c0bbc5e
Stason4ik <gromovstas64@gmail.com>
2023-10-19 19:11:26 +0300

    Изменил родительский класс OptionsMiddleware, добавив  обобщение под тип  ILogger<T> Изменил в последующем все дочерние классы

--- WebApplication8/Middleware/OptionsMiddleware.cs
+++ WebApplication8/Middleware/OptionsMiddleware.cs
@@ -3,22 +3,35 @@ using WebApplication8.Entity.Repository;
 
 namespace WebApplication8.Middleware
 {
-    public abstract class OptionsMiddleware
+    public abstract class OptionsMiddleware<T>
     {
         private IUserRepository _usersRepository;
         private IErorHandler _erorHandler;
         private RequestDelegate _nextMiddleware;
+        private ILogger<T> _logger;
         public IErorHandler ErorHandler { get => _erorHandler; set => _erorHandler = value; }
         public IUserRepository UsersRepository { get => _usersRepository; set => _usersRepository = value; }
         public RequestDelegate Next { get => _nextMiddleware; set => _nextMiddleware = value; }
+        public ILogger<T> Logger { get => _logger; set => _logger = value; }
 
-        public OptionsMiddleware(RequestDelegate nextMiddleware, IUserRepository usersRepository, IErorHandler erorHandler)
+        public OptionsMiddleware(RequestDelegate nextMiddleware,
+                                 IUserRepository usersRepository,
+                                 IErorHandler erorHandler,
+                                 ILogger<T> logger)
         {
             _usersRepository = usersRepository;
             _erorHandler = erorHandler;
             _nextMiddleware = nextMiddleware;
+            _logger = logger;
         }
 
-        public abstract Task InvokeAsync(HttpContext context);       
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Logger.LogInformation("The client sent a request along the path: " + context.Request.Path);
+            await ProccesingRequest(context);
+        }
+
+        public abstract Task ProccesingRequest(HttpContext context);
+      
     }
 }
